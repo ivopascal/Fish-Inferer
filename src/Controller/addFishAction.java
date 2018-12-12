debbb89980ca3c2fdf7ae45e3d6f8de0aabd0c11
HelmaK <HelmaK@github.com>
2018-12-12 11:46:59 +0100

    fixed shirt again

---
+++ src/Controller/addFishAction.java
@@ -0,0 +1,23 @@
+import javax.swing.AbstractAction;
+import javax.swing.JFrame;
+import java.awt.event.ActionEvent;
+
+/**
+	This simply handles the action once the addFishButton is clicked. The frame holds the relevant subsecuent actions to perform, and sends the string to the model.addFishByString(FishName)
+*/
+
+///should be in Controller.
+public class addFishAction extends AbstractAction
+{
+	private Model model;
+	public addFishAction(Model m){
+		this.model = m;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e){
+		JFrame addFishFrame = new addFishFrame(model);
+	}
+
+
+}
