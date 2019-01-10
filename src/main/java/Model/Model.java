5dc6191a3fece8cbcaba36de8040c6050292033f
Travis Hammond <dashdeckers@gmail.com>
2019-01-10 13:04:23 +0100

    output prints to gui now. its a bit small though?

--- src/main/java/Model/Model.java
+++ src/main/java/Model/Model.java
@@ -14,16 +14,17 @@ public class Model {
 	// Parameters
 	private int aquarium_width;
 	private int aquarium_length;
-	private int aquarium_depth;
+	private int aquarium_height;
 	private String[] infoButtonStrings = {"Why are my fish dying?", "Algae Info", "Stress", "Lighting"};
-	private String[] parameterStrings = {"Temp  ", "pH  ", "GH  ", "X  ", "Y  ", "Z  "};
+	private String[] parameterStrings = {"Temp  ", "pH  ", "GH  ", "Nitrite  ", "Nitrate  ", "Chlorine  "};
 	private String[] fishStrings = {"Red Crystal Shrimp", "Red Cherry Shrimp", "Pleco",
 			"Moon fish", "Platy", "Guppy (M)", "Guppy (F)" , "Fire neon", "Endler", "Cardinal", "Betta (M)", "Betta (F)" , "Corydora", "Goldfish", "Molly"};
 
 	private ArrayList<Fish> fish = new ArrayList<Fish>();
 	private ArrayList<Param> parameters = new ArrayList<Param>();
 	private Fishtank tankPanel;
-	private Output printer = new Output();
+
+	public Output printer = new Output();
 
 	public Model() {
 		makeParameters();
@@ -100,14 +101,10 @@ public class Model {
 					if (p.name.equals("Temp  ") && p.value != null) {
 						if (p.value > f.getMaxTemp() || p.value < f.getMinTemp())
 							printer.addWaterWarning("TEMP", f, p.value);
-//							System.out.println("WARNING: For " + f.getFishName() + ", the Temp is " + p.value +
-//									", but should be between " + f.getMinTemp() + " and " + f.getMaxTemp());
 					}
 					if (p.name.equals("pH  ") && p.value != null) {
 						if (p.value > f.getMaxpH() || p.value < f.getMinpH())
 							printer.addWaterWarning("PH", f, p.value);
-//							System.out.println("WARNING: For " + f.getFishName() + ", the pH is " + p.value +
-//									", but should be between " + f.getMinpH() + " and " + f.getMaxpH());
 					}
 				}
 			}
@@ -128,7 +125,6 @@ public class Model {
 						// if there is another fish that could eat this fish, print a warning
 						if (f.getPredators().contains(f2.getFishName()))
 							printer.addSocialWarning(f, f2);
-//							System.out.println("WARNING: The " + f2.getFishName() + " will eat the " + f.getFishName() + "!");
 					}
 				}
 				// clear the second duplicate check, else we will skip some fish on the next outer for loop iteration
@@ -137,74 +133,7 @@ public class Model {
 				int fishCount = Collections.frequency(fish, f);
 				if (fishCount > f.getMaxGroupSize() || fishCount < f.getMinGroupSize())
 					printer.addSocialWarning(f, fishCount);
-//					System.out.println("WARNING: There are currently " + fishCount + " " + f.getFishName() + "'s, but they need " +
-//							"to be in a group of at least " + f.getMinGroupSize() + " and at most " + f.getMaxGroupSize());
 			}
 		}
 	}
 }
-
-
-
-
-
-
-
-
-
-
-/*
-	public boolean canAddFish(String FishName)
-	{
-		Fish compfish = new Fish(FishName);
-		System.out.println("Number of fish: " + mrBubbles.size());
-		
-		for(Fish f: mrBubbles){
-			if(     compfish.getMinTemp() >= f.getMaxTemp()){
-				System.out.println("Can't add " + FishName + " because " + f.fishName + " prefers colder water.");
-				return false;
-			}
-			if(	compfish.getMaxTemp() <= f.getMinTemp()){
-				System.out.println("Can't add " + FishName + " because " + f.fishName + " prefers warmer water.");
-                                return false;
-
-			}
-			if(	compfish.getMinpH() >= f.getMaxpH()){
-				System.out.println("Can't add " + FishName + " because " + f.fishName + " prefers lower pH.");
-                                return false;
-
-			}
-			if(	compfish.getMaxpH() <= f.getMinpH()){
-				System.out.println("Can't add " + FishName + " because " + f.fishName + " prefers higher pH.");
-				return false;
-			}
-		}
-		return true;
-	}
-
-	public String canAddFishProblems(String FishName){
-                Fish compfish = new Fish(FishName);
-                System.out.println("Number of fish: " + mrBubbles.size());
-
-		String output = "";
-                for(Fish f: mrBubbles){
-                        if(     compfish.getMinTemp() >= f.getMaxTemp()){
-                                System.out.println("Can't add " + FishName + " because " + f.fishName + " prefers colder water.");
-                                output = output + "Can't add " + FishName + " because " + f.fishName + " prefers colder water." + "\n";
-                        }
-                        if(     compfish.getMaxTemp() <= f.getMinTemp()){
-                                System.out.println("Can't add " + FishName + " because " + f.fishName + " prefers warmer water.");
-				output = output + "Can't add " + FishName + " because " + f.fishName + " prefers warmer water." + "\n";
-                        }
-                        if(     compfish.getMinpH() >= f.getMaxpH()){
-                                System.out.println("Can't add " + FishName + " because " + f.fishName + " prefers lower pH.");
-				output = output + "Can't add " + FishName + " because " + f.fishName + " prefers lower pH." + "\n";
-                        }
-                        if(     compfish.getMaxpH() <= f.getMinpH()){
-                                System.out.println("Can't add " + FishName + " because " + f.fishName + " prefers higher pH.");
-				output = output + "Can't add " + FishName + " because " + f.fishName + " prefers lower pH." + "\n";
-                        }
-                }
-                return output;
-        }
-*/
